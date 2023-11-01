5b039f90bcfe6f37fda9a2c2c4fd59a36b4b72c4
wh1tly337 <wh1tly337@gmail.com>
2023-11-01 15:48:34 +0500

    added all working links to pages

--- SneakerStep/main/urls.py
+++ SneakerStep/main/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('catalog', views.catalog)
+    path('', views.home, name='home'),
+    path('catalog', views.catalog, name='catalog'),
+    path('about_us', views.about_us, name='about_us'),
+    path('chect_out', views.chect_out, name='chect_out'),
+    path('comming_soon_page', views.comming_soon_page, name='comming_soon_page'),
+    path('contact_us', views.contact_us, name='contact_us'),
+    path('product_card', views.product_card, name='product_card'),
+    path('your_cart', views.your_cart, name='your_cart')
 ]
--- SneakerStep/main/views.py
+++ SneakerStep/main/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 
 
-def index(request):
+def home(request):
     return render(template_name='main/Home.html', request=request)
 
 
-def catalog():
-    pass
+def catalog(request):
+    return render(template_name='main/Catalog.html', request=request)
+
+
+def about_us(request):
+    return render(template_name='main/About_us.html', request=request)
+
+
+def chect_out(request):
+    return render(template_name='main/Chect_out.html', request=request)
+
+
+def comming_soon_page(request):
+    return render(template_name='main/Comming_soon_page.html', request=request)
+
+
+def contact_us(request):
+    return render(template_name='main/Contact_us.html', request=request)
+
+
+def product_card(request):
+    return render(template_name='main/Product_card.html', request=request)
+
+
+def your_cart(request):
+    return render(template_name='main/Your_cart.html', request=request)
